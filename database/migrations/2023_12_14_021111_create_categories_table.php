65644127ef093c0dbc5b19d603b4a5e85a218979
GokaiInfinity <patriciofilbert@gmail.com>
2023-12-15 01:00:15 +0700

    timestamps(0)

--- database/migrations/2023_12_14_021111_create_categories_table.php
+++ database/migrations/2023_12_14_021111_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id("category_id");
             $table->string("category_name");
-            $table->timestamps();
+            $table->timestamps(0);
         });
     }
 
--- database/migrations/2023_12_14_021250_create_brands_table.php
+++ database/migrations/2023_12_14_021250_create_brands_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id("brand_id");
             $table->string("brand_name");
-            $table->timestamps();
+            $table->timestamps(0);
         });
     }
 
