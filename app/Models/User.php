18073ccefc8457c47b024f9c471986e48b6498f1
JDTOmoi <jasondavetahir@gmail.com>
2023-12-24 18:09:34 +0700

    added HasMany class for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
