2ac173147695a20d1b23027d1777b180957cc5fd
JDTOmoi <jasondavetahir@gmail.com>
2024-01-05 22:56:19 +0700

    removed null errors

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -44,30 +44,6 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <ul class="navbar-nav d-md-none">
-                          @if(Auth::user()->isAdmin())
-                            <li class="nav-item {{$activeProduk ?? ''}}">
-                              <a class="nav-link {{$activeProduk ?? ''}}" href="{{route('admin.daftarproduk')}}">Produk</a>
-                            </li>
-                            <li class="nav-item {{$activeBrand ?? ''}}">
-                              <a class="nav-link {{$activeBrand ?? ''}}" href="{{route('admin.daftarbrand')}}">Brand</a>
-                            </li>
-                            <li class="nav-item {{$activeCategory ?? ''}}">
-                              <a class="nav-link {{$activeCategory ?? ''}}" href="{{route('admin.daftarcategory')}}">Kategori</a>
-                            </li>
-                            <li class="nav-item {{$activeTransaction ?? ''}}">
-                              <a class="nav-link {{$activeTransaction ?? ''}}" href="{{route('admin.kirimtransaksi')}}">Send Transaction</a>
-                            </li>
-                            <li class="nav-item {{$activeOrder ?? ''}}">
-                              <a class="nav-link {{$activeOrder ?? ''}}" href="{{route('tambahorder')}}">Order</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link {{$activePort ?? ''}}" href="{{route('admin.portofolio')}}">Portofolio</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link {{$activeNews ?? ''}}" href="{{route('admin.berita')}}">Berita</a>
-                            </li>
-                            
-                          @else
                             <li class="nav-item {{$activeProduk ?? ''}}">
                               <a class="nav-link {{$activeProduk ?? ''}}" href="{{route('produk')}}">Produk</a>
                             </li>
@@ -80,7 +56,6 @@
                             <li class="nav-item {{$activeNews ?? ''}}">
                               <a class="nav-link {{$activeNews ?? ''}}" href="{{route('berita')}}">Berita</a>
                             </li>
-                          @endif
                           <li class="nav-item">
                             <a class="nav-link {{$activeContact ?? ''}}" href="{{route('contact')}}">Kontak</a>
                           </li>
@@ -130,30 +105,6 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
-                @if(Auth::user()->isAdmin())
-                <li class="nav-item {{$activeProduk ?? ''}}">
-                  <a class="nav-link {{$activeProduk ?? ''}}" href="{{route('admin.daftarproduk')}}">Produk</a>
-                </li>
-                <li class="nav-item {{$activeBrand ?? ''}}">
-                  <a class="nav-link {{$activeBrand ?? ''}}" href="{{route('admin.daftarbrand')}}">Brand</a>
-                </li>
-                <li class="nav-item {{$activeCategory ?? ''}}">
-                  <a class="nav-link {{$activeCategory ?? ''}}" href="{{route('admin.daftarcategory')}}">Kategori</a>
-                </li>
-                <li class="nav-item {{$activeTransaction ?? ''}}">
-                  <a class="nav-link {{$activeTransaction ?? ''}}" href="{{route('admin.kirimtransaksi')}}">Send Transaction</a>
-                </li>
-                <li class="nav-item {{$activeOrder ?? ''}}">
-                  <a class="nav-link {{$activeOrder ?? ''}}" href="{{route('tambahorder')}}">Order</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link {{$activePort ?? ''}}" href="{{route('admin.portofolio')}}">Portofolio</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link {{$activeNews ?? ''}}" href="{{route('admin.berita')}}">Berita</a>
-                </li>
-                
-              @else
                 <li class="nav-item {{$activeProduk ?? ''}}">
                   <a class="nav-link {{$activeProduk ?? ''}}" href="{{route('produk')}}">Produk</a>
                 </li>
@@ -166,7 +117,6 @@
                 <li class="nav-item {{$activeNews ?? ''}}">
                   <a class="nav-link {{$activeNews ?? ''}}" href="{{route('berita')}}">Berita</a>
                 </li>
-              @endif
               <li class="nav-item">
                 <a class="nav-link {{$activeContact ?? ''}}" href="{{route('contact')}}">Kontak</a>
               </li>
