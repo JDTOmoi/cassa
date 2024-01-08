6e679c98e389d7bc4d2a1357141a13c68ce4f3bf
JDTOmoi <jasondavetahir@gmail.com>
2024-01-08 09:48:55 +0700

    modified email for mailing system tests

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
 
         DB::table('users')->insert(
-            ['name' => 'Test','Email' => 'test@gmail.com','password' => bcrypt('12345678'), 'role_id' => 2, 'is_login'=>'0', 'is_active'=>'1','remember_token'=>null]
+            ['name' => 'Test','Email' => 'iluvgemz60@gmail.com','password' => bcrypt('12345678'), 'role_id' => 2, 'is_login'=>'0', 'is_active'=>'1','remember_token'=>null]
         );
     }
 }
