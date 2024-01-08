dce0336912f53cab85250585c22eee7cea8a4dad
JDTOmoi <jasondavetahir@gmail.com>
2024-01-08 13:45:47 +0700

    changed the images, again!

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('storage/images'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
